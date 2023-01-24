323913f2be7fcbce4bb6d68b1080b165b524ea00
VJS <minsung_96@naver.com>
2023-01-24 23:58:01 +0900

    NAvbar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -53,6 +53,10 @@ const Sidebar = () => {
             />
           ))}
         </div>
+        <Icon 
+          styles="bg-[#1c1c24] shadow-secondary"
+          imgUrl={sun}
+        />
       </div>
     </div>
   )
